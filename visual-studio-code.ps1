4838eaaca69db38f9d0b7aa809eb210f9df0595d
Maxime Labelle <springcomp@users.noreply.github.com>
2021-09-11 11:26:10 +0200

    Install VS Code and extensions.

---
+++ visual-studio-code.ps1
@@ -0,0 +1,74 @@
+Function Install-VsCode {
+	[CmdletBinding()]
+	param()
+
+	BEGIN{
+        $address = "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user"
+		$exe = "$Env:TEMP\VsCodeUserSetup_x64.exe"
+	}
+	PROCESS{
+
+        Invoke-RestMethod `
+            -Method Get `
+            -Uri $address `
+            -OutFile $exe
+
+		$job = Start-Job `
+            -ArgumentList $exe `
+            -ScriptBlock {
+                param([string]$exe)
+			    $expression = ". `"$($exe)`" /verysilent /norestart /mergetasks=!runcode"
+                Write-Host $expression
+			    Invoke-Expression -Command $expression
+			    [Threading.Thread]::Sleep(10000)
+		    }
+
+		Wait-Job $job
+		Receive-Job $job
+		Remove-Job $job
+	}
+	END {
+
+		$code = "$Env:LOCALAPPDATA\Programs\Microsoft VS Code\bin\code.cmd"
+		if (Test-Path -Path $code) {
+			Write-Host "Visual Studio Code installed successfully." -ForegroundColor Cyan
+			return $true
+		} else {
+			Write-Host "Unable to install Visual Studio Code." -ForegroundColor Red
+			return $false
+		}
+	}
+}
+
+Function Install-VsCodeExtensions {
+
+	$code = "$Env:LOCALAPPDATA\Programs\Microsoft VS Code\bin\code.cmd"
+	$extensions = 
+		"DotJoshJohnson.xml", `
+		"formulahendry.dotnet-test-explorer", `
+		"ms-azuretools.vscode-azurefunctions", `
+		"ms-azuretools.vscode-docker", `
+		"MS-CEINTL.vscode-language-pack-fr", `
+		"ms-dotnettools.csharp", `
+		"ms-vscode-remote.remote-containers", `
+		"ms-vscode-remote.remote-ssh", `
+		"ms-vscode-remote.remote-ssh-edit", `
+		"ms-vscode-remote.remote-ssh-explorer", `
+		"ms-vscode-remote.remote-wsl", `
+		"ms-vscode-remote.vscode-remote-extensionpack", `
+		"ms-vscode.azure-account", `
+		"ms-vscode.powershell", `
+		"slevesque.vscode-hexdump", `
+		"TqrHsn.vscode-docker-registry-explorer", `
+		"Tyriar.sort-lines", `
+		"vscodevim.vim" |% {
+
+			Write-Host "Installing $_ Visual Studio Code extension..." -ForegroundColor DarkGray
+			. $code --install-extension $_
+		
+		}
+}
+
+if (Install-VsCode) {
+    Install-VsCodeExtensions
+}
