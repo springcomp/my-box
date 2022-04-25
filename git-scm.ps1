ef82f85d1490d07a3dbc433f91510ae4cefa93fc
Maxime LABELLE <maxime.labelle@chanel.com>
2022-04-25 16:44:57 +0200

    Supports installation of Git.

---
+++ git-scm.ps1
@@ -0,0 +1,39 @@
+Function Install-Git {
+	[CmdletBinding()]
+	param()
+
+	BEGIN {
+
+		$address = "https://github.com/git-for-windows/git/releases/download/v2.36.0.windows.1/Git-2.36.0-64-bit.exe"
+		$exe = "$Env:TEMP\git-scm.exe"
+	}
+
+	PROCESS {
+
+		Invoke-RestMethod `
+			-Method Get `
+			-Uri $address `
+			-OutFile $exe
+
+		$job = Start-Job `
+			-ArgumentList $exe `
+			-ScriptBlock {
+			param([string]$exe)
+
+			## https://github.com/git-for-windows/git/wiki/Silent-or-Unattended-Installation
+
+			$options = "gitlfs,assoc,assoc_sh,windowsterminal"
+
+			$expression = ". `"$($exe)`" /VERYSILENT /NORESTART /NOCANCEL /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /COMPONENTS=`"$options`" "
+			Write-Hos $expression
+			Invoke-Expression -Command $expression
+			[Threading.Thread]::Sleep(50000)
+		}
+
+		Wait-Job $job
+		Receive-Job $job
+		Remove-Job $job
+	}
+}
+
+Install-Git
