f49c72cb033834c2bbb57ceb7f3b9f88b6d4fd09
Maxime LABELLE <maxime.labelle@chanel.com>
2022-04-25 16:52:48 +0200

    Run installation from a top-level script.

---
+++ bootstrap.ps1
@@ -0,0 +1,4 @@
+iex "& { $(irm 'https://raw.githubusercontent.com/springcomp/my-box/main/bootstrap/powershell.ps1') }"
+
+iex "& { $(irm 'https://raw.githubusercontent.com/springcomp/my-box/main/git.ps1') }"
+iex "& { $(irm 'https://raw.githubusercontent.com/springcomp/my-box/main/visual-studio-code.ps1') }"
